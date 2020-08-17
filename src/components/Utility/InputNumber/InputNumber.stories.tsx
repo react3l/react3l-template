055eea4d310aa6757096f801af58dfcde6a371e3
Le Quang Anh <leanh2194@gmail.com>
2020-08-17 18:45:06 +0700

    add input number + material input

--- src/components/Utility/InputNumber/InputNumber.stories.tsx
+++ src/components/Utility/InputNumber/InputNumber.stories.tsx
@@ -1,10 +1,60 @@
 import React from 'react';
 import {storiesOf} from '@storybook/react';
 import nameof from 'ts-nameof.macro';
-import { InputNumber } from 'antd';
+import InputNumber, { DECIMAL, LONG } from './InputNumber';
+import { Radio } from 'antd';
+import { RadioChangeEvent } from 'antd/lib/radio';
+
 
 function Default() {
-    return <InputNumber />;
+    const [value, setValue] = React.useState(null);
+
+    const [numberType, setNumberType] = React.useState<string>(LONG);
+
+    const [isReverse, setIsReverse] = React.useState(false);
+
+    const [isMaterial, setIsMaterial] = React.useState(false);
+
+    const handleChangeType = React.useCallback((event: RadioChangeEvent) => {
+        setNumberType(event.target.value);
+        setValue(undefined);
+    }, []);
+
+    const handleChangeSeperation = React.useCallback((event: RadioChangeEvent) => {
+        setIsReverse(event.target.value);
+        setValue(undefined);
+    }, []);
+
+    const handleChangeStyle = React.useCallback((event: RadioChangeEvent) => {
+        setIsMaterial(event.target.value);
+        setValue(undefined);
+    }, []);
+
+    return <div style={{width: '250px', margin: '10px', backgroundColor: '#F2F2F2'}}>
+        <InputNumber placeHolder={'Enter number...'}
+            isMaterial={isMaterial}
+            value={value} 
+            numberType={numberType} 
+            isReverseSymb={isReverse}/>
+        <div style={{margin: '10px', width: '300px'}}>
+                    <Radio.Group onChange={handleChangeType} value={numberType}>
+                        <Radio value={DECIMAL}>Decimal</Radio>
+                        <Radio value={LONG}>Long</Radio>
+                    </Radio.Group>
+        </div>
+        <div style={{margin: '10px', width: '300px'}}>
+                    <Radio.Group onChange={handleChangeSeperation} value={isReverse}>
+                        <Radio value={true}>Reverse Seperation</Radio>
+                        <Radio value={false}>Normal</Radio>
+                    </Radio.Group>
+        </div>
+        <div style={{margin: '10px', width: '300px'}}>
+                    <Radio.Group onChange={handleChangeStyle} value={isMaterial}>
+                        <Radio value={true}>Material Style</Radio>
+                        <Radio value={false}>Normal Style</Radio>
+                    </Radio.Group>
+        </div>
+    </div>;
 }
   
 storiesOf('InputNumber', module)
--- src/components/Utility/InputNumber/InputNumber.tsx
+++ src/components/Utility/InputNumber/InputNumber.tsx
@@ -1,11 +1,170 @@
-import React from 'react';
+import React, { RefObject } from 'react';
 import './InputNumber.scss';
+import { Model } from 'react3l/core';
+import classNames from 'classnames';
 
-function InputNumber() {
+export const DECIMAL: string = 'DECIMAL';
+export const LONG: string = 'LONG';
+
+interface InputNumber<T extends Model> {
+  value?: number;
+  isMaterial?: boolean;
+  numberType?: string;
+  isReverseSymb?: boolean;
+  decimalDigit?: number;
+  placeHolder?: string;
+  className?: string;
+  onChange?: (T: number) => void;
+}
+
+function InputNumber(props: InputNumber<Model>) {
+  const {
+    value,
+    isMaterial,
+    numberType,
+    decimalDigit,
+    isReverseSymb,
+    placeHolder,
+    className,
+    onChange,
+  } = props;
+
+  const [internalValue, setInternalValue] = React.useState<string>('');
+
+  const inputRef: RefObject<HTMLInputElement> = React.useRef<HTMLInputElement>(null);
+
+  const buildRegex = React.useCallback(() => {
+    var regExDecimal = '';
+    var regExString = '';
+    for (let i=1; i <= decimalDigit; i++) {
+      regExDecimal += '\\d';
+    }
+    if (isReverseSymb) {
+      regExString = '(\\d)(?=(?:\\d{3})+(?:,|$))|(,' + regExDecimal + '?)\\d*$';
+    } else {
+      regExString = '(\\d)(?=(?:\\d{3})+(?:\\.|$))|(\\.' + regExDecimal + '?)\\d*$';
+    }
+    return new RegExp(regExString, 'g');
+  }, [decimalDigit, isReverseSymb]);
+
+  const formatString = React.useCallback((inputValue: string): string => {
+    const newRegEx = buildRegex();
+    if (isReverseSymb) {
+      switch (numberType) {
+        case DECIMAL:
+          inputValue = inputValue.replace(/[^0-9,]/g, "");
+          return (inputValue).replace(newRegEx, 
+            (m, s1, s2) => {
+              return s2 || (s1 + '.');
+            },
+          );
+        default:
+          inputValue = inputValue.replace(/[^0-9]/g, "");
+          return inputValue.replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+      }
+    } else {
+      switch (numberType) {
+        case DECIMAL:
+          inputValue = inputValue.replace(/[^0-9.]/g, "");
+          return (inputValue).replace(newRegEx, 
+            (m, s1, s2) => {
+              return s2 || (s1 + ',');
+            },
+          );
+        default:
+          inputValue = inputValue.replace(/[^0-9]/g, "");
+          return inputValue.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+      }
+    }
+  }, [isReverseSymb, numberType, buildRegex]);
+
+  const parseNumber = React.useCallback((value: string): [number, boolean] => {
+    var isOutOfRange, number, stringValue;
+    if (isReverseSymb) {
+      stringValue = value.replace(/[,.]/g, m => (m === '.' ? ',' : '.'));
+      stringValue = stringValue.replace(/,/g, '');
+    } else {
+      stringValue = value.replace(/,/g, '');
+    }
+    switch(numberType) {
+      case DECIMAL:
+        isOutOfRange = stringValue.length > 21 ? true : false;
+        number = parseFloat(stringValue);
+        return [number, isOutOfRange];
+      default:
+        isOutOfRange = stringValue.length > 17 ? true : false;
+        number = parseInt(stringValue);
+        return [number, isOutOfRange];
+    }
+  }, [numberType, isReverseSymb]);
+
+  const handleChange = React.useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
+    const stringValue = formatString(event.target.value);
+    const [numberValue, isOutOfRange] = parseNumber(stringValue);
+    if (!isOutOfRange) {
+      setInternalValue(stringValue);
+      if (typeof onChange === 'function') {
+        onChange(numberValue);
+      }
+    }
+  }, [formatString, parseNumber, onChange]);
+
+  const handleClearInput = React.useCallback((event: React.MouseEvent<HTMLElement, MouseEvent>) => {
+    setInternalValue('');
+    if (typeof onChange === 'function') {
+      onChange(null);
+    }
+    inputRef.current.focus();
+  }, [onChange]);
+
+  React.useEffect(() => {
+    if (value) {
+      const stringValue = formatString('' + value);
+      setInternalValue(stringValue);
+    } else {
+      setInternalValue('');
+    }
+  }, [value, formatString]);
+
   return (
     <>
+      <div className="input-number__container">
+        { isMaterial ? 
+          <div className="material__input">
+            <label>
+              <input type="text"
+                value={internalValue}
+                onChange={handleChange}
+                ref={inputRef} />
+              <span className="placeholder">{placeHolder ? placeHolder : 'Enter number...'}</span>
+              { internalValue ? 
+                <i className="tio-clear" onClick={handleClearInput}></i> :
+                <i className={classNames(className)}></i>
+              }  
+            </label>
+          </div> :
+          <>
+            <input type="text"
+              value={internalValue}
+              onChange={handleChange}
+              placeholder={placeHolder}
+              ref={inputRef} 
+              className="component__input"/>
+            {internalValue && <i className="input-number__icon tio-clear" onClick={handleClearInput}></i>}
+          </>
+        }
+
+      </div>
     </>
   );
 }
+
+InputNumber.defaultProps = {
+  isReverseSymb: false,
+  numberType: LONG,
+  decimalDigit: 4,
+  isMaterial: true,
+  className: 'tio-dollar_outlined',
+};
 
 export default InputNumber;
