2dc9aef1c2ff757fe59717620cea670c63db2df2
huongnt <huongnguyenhd96@gmail.com>
2020-08-27 17:44:28 +0700

    Them footer

--- src/components/AppFooter/AppFooter.tsx
+++ src/components/AppFooter/AppFooter.tsx
@@ -1,9 +1,34 @@
-import React from 'react';
-import './AppFooter.scss';
+import React from "react";
+import "./AppFooter.scss";
+import { Steps, Row, Col } from "antd";
 
+const { Step } = Steps;
 function AppFooter() {
   return (
-    <footer>
+    <footer className="app-footer">
+      <Row className="d-flex align-items-center mt-3 mb-3">
+        <Col lg={18}>
+          <div className="app-footer__steps">
+            <Steps current={1} size="small">
+              <Step title="Finished" />
+              <Step />
+              <Step />
+              <Step />
+              <Step />
+              <Step />
+              <Step />
+            </Steps>
+          </div>
+        </Col>
+        <Col lg={6}>
+          <div className="app-footer__actions d-flex justify-content-end">
+            <button className="btn component__btn-primary mr-4">Gửi</button>
+            <button className="btn component__btn-save-template mr-4">Lưu mẫu</button>
+            <button className="btn component__btn-save-draft mr-4"><span>Lưu nháp</span></button>
+            <button className="btn component__btn-cancel mr-5">Hủy</button>
+          </div>
+        </Col>
+      </Row>
     </footer>
   );
 }
--- src/views/App/App.tsx
+++ src/views/App/App.tsx
@@ -4,7 +4,7 @@ import AppHeader from 'components/AppHeader/AppHeader';
 import { menu } from 'config/menu';
 import { routes } from 'config/routes';
 import React from 'react';
-import { Switch, withRouter } from 'react-router';
+import { Switch, withRouter, useLocation } from 'react-router';
 import { renderRoutes } from 'react-router-config';
 import './App.scss';
 import { useGlobal, setGlobal } from 'reactn';
@@ -16,32 +16,46 @@ function App() {
 
   const [display] = useGlobal<GlobalState>('display');
   const [toggleMenu] = useGlobal<GlobalState>('toggle');
+  const [displayFooter, setDisplayFooter] = React.useState<boolean>(false);
+
+  const { pathname } = useLocation();
+
+  React.useEffect(() => {
+    if (pathname.includes('detail')) {
+      setDisplayFooter(true);
+    }
+  }, [pathname]);
   const handleOffOverlay = React.useCallback(() => {
     setGlobal<GlobalState>({ display: false });
   }, []);
 
   return (
-    <div className="app d-flex">
+    <>
+      <div className="app d-flex">
 
-      <AppAsideCollapse className={toggleMenu ? 'slide-in' : 'slide-out'} routes={menu} />
-      <AppAside className={!toggleMenu ? 'app_slide-in' : 'app_slide-out'} routes={menu} />
+        <AppAsideCollapse className={toggleMenu ? 'slide-in' : 'slide-out'} routes={menu} />
+        <AppAside className={!toggleMenu ? 'app_slide-in' : 'app_slide-out'} routes={menu} />
 
-      <div
-        className={classNames(
-          { "header__overlay header__display-block": display },
-        )}
-        onClick={handleOffOverlay}
-      >
+        <div
+          className={classNames(
+            { "header__overlay header__display-block": display },
+          )}
+          onClick={handleOffOverlay}
+        >
+        </div>
+        <section className={classNames("flex-item", (!toggleMenu ? 'main content-in' : 'main content-out'))}>
+          <AppHeader />
+          <main className="body">
+            <Switch>{renderRoutes(routes)}</Switch>
+          </main>
+        </section>
+        {
+          displayFooter === true && (<AppFooter />)
+        }
 
       </div>
-      <section className={classNames("flex-item", (!toggleMenu ? 'main content-in' : 'main content-out'))}>
-        <AppHeader />
-        <main className="body">
-          <Switch>{renderRoutes(routes)}</Switch>
-        </main>
-      </section>
-      <AppFooter />
-    </div>
+    </>
+
   );
 }
 
