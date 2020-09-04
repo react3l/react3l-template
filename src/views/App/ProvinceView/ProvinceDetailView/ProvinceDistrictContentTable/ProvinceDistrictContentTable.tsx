a8b3ac0728b616571ffced6fa3bbf4bb9a302950
Nam <namnh21894@gmail.com>
2020-09-04 22:16:13 +0700

    remove logic in province content table

--- src/views/App/ProvinceView/ProvinceDetailView/ProvinceDistrictContentTable/ProvinceDistrictContentTable.tsx
+++ src/views/App/ProvinceView/ProvinceDetailView/ProvinceDistrictContentTable/ProvinceDistrictContentTable.tsx
@@ -1,14 +1,13 @@
-import React from 'react';
-import './ProvinceDistrictContentTable.scss';
-import {formService} from 'services/FormService';
-import {Province} from 'models/Province';
-import {District} from 'models/District';
-import {Model} from 'react3l/core';
-import Table, {ColumnProps} from 'antd/lib/table';
-import {useTranslation} from 'react-i18next';
-import nameof from 'ts-nameof.macro';
+import { District } from "models/District";
+import { Province } from "models/Province";
+import React from "react";
+import { Model } from "react3l/core";
+import "./ProvinceDistrictContentTable.scss";
 
-interface ProvinceDistrictContentTableProps<T extends Model, TContent extends Model> {
+interface ProvinceDistrictContentTableProps<
+  T extends Model,
+  TContent extends Model
+> {
   parentModel: T;
 
   onChangeSimpleField: (fieldName: keyof T) => (fieldValue: T[keyof T]) => void;
@@ -16,58 +15,10 @@ interface ProvinceDistrictContentTableProps<T extends Model, TContent extends Mo
   fieldName: keyof T;
 }
 
-function ProvinceDistrictContentTable(props: ProvinceDistrictContentTableProps<Province, District>) {
-  const [translate] = useTranslation();
-
-  const {parentModel, onChangeSimpleField} = props;
-
-  const [
-    districts,
-    handleChangeDistrictField,
-  ] = formService.useContentField<Province, District>(District, parentModel, 'districts', onChangeSimpleField);
-
-  const columns: ColumnProps<District>[] = React.useMemo(
-    () => [
-      {
-        title: translate('district.id'),
-        key: nameof(districts[0].id),
-        dataIndex: nameof(districts[0].id),
-        render(id: number, district: District, index: number) {
-          return (
-            <input
-              type="text"
-              className="form-control"
-              value={id}
-              onChange={handleChangeDistrictField(index, 'id')}
-            />
-          );
-        },
-      },
-      {
-        title: translate('district.name'),
-        key: nameof(districts[0].name),
-        dataIndex: nameof(districts[0].name),
-        render(name: string, district: District, index: number) {
-          return (
-            <input
-              type="text"
-              className="form-control"
-              value={name}
-              onChange={handleChangeDistrictField(index, 'name')}
-            />
-          );
-        },
-      },
-    ],
-    [districts, handleChangeDistrictField, translate],
-  );
-
-  return (
-    <Table
-      dataSource={districts}
-      columns={columns}
-    />
-  );
+function ProvinceDistrictContentTable(
+  props: ProvinceDistrictContentTableProps<Province, District>,
+) {
+  return <></>;
 }
 
 export default ProvinceDistrictContentTable;
