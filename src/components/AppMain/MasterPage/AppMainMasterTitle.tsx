99a3337399491579c9a195288f9459ee8b774fe2
Chi Nguyen Ngoc <ngocchi.fit148.vnu@gmail.com>
2021-03-02 16:22:08 +0700

    realign component in app header and change list action to dropdown

--- src/components/AppMain/MasterPage/AppMainMasterTitle.tsx
+++ src/components/AppMain/MasterPage/AppMainMasterTitle.tsx
@@ -1,32 +1,21 @@
-import { TFunction } from 'i18next';
-import React from 'react';
-import { ReactNode } from 'react';
-import { UseMaster } from 'services/pages/master-service';
+import { TFunction } from "i18next";
+import React from "react";
+import { ReactNode } from "react";
+import { UseMaster } from "services/pages/master-service";
 
 export interface AppMainMasterTitle extends UseMaster {
-    translate?: TFunction,
-    children?: ReactNode
+  translate?: TFunction;
+  children?: ReactNode;
 }
 
 export function AppMainMasterTitle(props: AppMainMasterTitle) {
+  const { translate, handleGoCreate, children } = props;
 
-    const {
-        translate,
-        handleGoCreate,
-        children
-    } = props;
-    
-    return <>
-        <div className='page__header d-flex align-items-center justify-content-between'>
-            <div className='page__title'>
-                {children}
-            </div>
-            <div className='page__actions d-flex align-items-center'>
-                <button className='btn btn-sm component__btn-primary ml-3 grow-animate-1'
-                        onClick={ handleGoCreate }>
-                    {translate("general.actions.create")}
-                </button>
-            </div>
-        </div>
-    </>;
+  return (
+    <>
+      <div className="page__header d-flex align-items-center justify-content-between">
+        <div className="page__title">{children}</div>
+      </div>
+    </>
+  );
 }
